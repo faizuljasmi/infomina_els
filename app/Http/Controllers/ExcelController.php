69bd5d15ef49696fb0f5b65c9f0d8d92cf5d2bbb
gobhinrao <gobhinrao18@gmail.com>
2020-03-24 02:57:17 +0800

    Reporting Module by Gobhin Rao

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -4,18 +4,104 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Imports\Import;
-use App\Exports\Export;
 use Maatwebsite\Excel\Facades\Excel;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use Illuminate\Http\Request;
+use Datatables;
 use DB;
 use App\User;
+use App\LeaveApplications;
 
 class ExcelController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-        return view('excel/transfer')->with('users', $users);
+        $users = User::sortable()
+        ->join('leave_applications', 'leave_applications.user_id', '=', 'users.id')
+        ->join('leave_types', 'leave_types.id', '=', 'leave_applications.leave_type_id')
+        ->select('users.*', 'leave_applications.*', 'leave_types.name as leave_type_name')
+        ->paginate(15);
+
+        $count_approve = User::join('leave_applications', 'leave_applications.user_id', '=', 'users.id')
+        ->where('leave_applications.status','like','%APPROVED%')
+        ->count();
+
+        $count_pending = User::join('leave_applications', 'leave_applications.user_id', '=', 'users.id')
+        ->where('leave_applications.status','like','%PENDING_1%')
+        ->orwhere('leave_applications.status','like','%PENDING_2%')
+        ->orwhere('leave_applications.status','like','%PENDING_3%')
+        ->count();
+
+        $count_reject = User::join('leave_applications', 'leave_applications.user_id', '=', 'users.id')
+        ->where('leave_applications.status','like','%DENIED_1%')
+        ->orwhere('leave_applications.status','like','%DENIED_2%')
+        ->orwhere('leave_applications.status','like','%DENIED_3%')
+        ->count();
+        
+        return view('excel/transfer')->with(compact('users', 'count_approve', 'count_pending', 'count_reject'));
+    }
+
+    public function search(Request $request)
+    {
+        $search_name = $request->get('name');
+        $date_from = $request->get('date_from');
+        $date_to = $request->get('date_to');
+        $leave_type = $request->get('leave_type');
+        $leave_status = $request->get('leave_status');
+
+        $count_approve = User::join('leave_applications', 'leave_applications.user_id', '=', 'users.id')
+        ->where('leave_applications.status','like','%APPROVED%')
+        ->count();
+
+        $count_pending = User::join('leave_applications', 'leave_applications.user_id', '=', 'users.id')
+        ->where('leave_applications.status','like','%PENDING_1%')
+        ->orwhere('leave_applications.status','like','%PENDING_2%')
+        ->orwhere('leave_applications.status','like','%PENDING_3%')
+        ->count();
+
+        $count_reject = User::join('leave_applications', 'leave_applications.user_id', '=', 'users.id')
+        ->where('leave_applications.status','like','%DENIED_1%')
+        ->orwhere('leave_applications.status','like','%DENIED_2%')
+        ->orwhere('leave_applications.status','like','%DENIED_3%')
+        ->count();
+
+        $query = User::sortable()
+        ->join('leave_applications', 'leave_applications.user_id', '=', 'users.id')
+        ->join('leave_types', 'leave_types.id', '=', 'leave_applications.leave_type_id')
+        ->select('users.*', 'leave_applications.*', 'leave_types.name as leave_type_name');
+
+        if($request->get('name') != '') {
+            $query->where('users.name','like','%'.$search_name.'%');
+        }
+
+        if($request->get('date_from') != '' && $request->get('date_to') != '') {
+            $query->wherebetween('leave_applications.date_from', [$date_from, $date_to]);
+        }
+
+        if($request->get('leave_type') != '') {
+            $query->where('leave_applications.leave_type_id','like','%'.$leave_type.'%');
+        }
+
+        if($request->get('leave_status') != '') {
+            if($request->get('leave_status') == 'PENDING') {
+                $query->where('leave_applications.status','like','%PENDING_1%');
+                $query->orwhere('leave_applications.status','like','%PENDING_2%');
+                $query->orwhere('leave_applications.status','like','%PENDING_3%');
+            }
+            if($request->get('leave_status') == 'DENIED') {
+                $query->where('leave_applications.status','like','%DENIED_1%');
+                $query->orwhere('leave_applications.status','like','%DENIED_2%');
+                $query->orwhere('leave_applications.status','like','%DENIED_3%');
+            }
+            else {
+                $query->where('leave_applications.status','like','%'.$leave_status.'%');
+            }
+        }
+
+        $users = $query->paginate(15);
+
+        return view('excel/transfer')->with(compact('users', 'search_name', 'date_from', 'date_to', 'leave_type', 'leave_status', 'count_approve', 'count_pending', 'count_reject'));
     }
 
     public function import(Request $request)
@@ -23,12 +109,78 @@ class ExcelController extends Controller
         $this->validate($request, [
             'import_file'  => 'required|mimes:xls,xlsx'
            ]);
+
         Excel::import(new Import(), request()->file('import_file'));
+
         return back()->with('success', 'Data imported successfully.');
     }
 
-    public function export()
+    public function export(Request $request)
     {
-        return Excel::download(new Export(), 'export.xlsx');
-    }
+        $search_name = $request->get('excel_search_name');
+        $date_from = $request->get('excel_date_from');
+        $date_to = $request->get('excel_date_to');
+        $leave_type = $request->get('excel_leave_type');
+        $leave_status = $request->get('excel_leave_status');
+
+        $query = User::join('leave_applications', 'leave_applications.user_id', '=', 'users.id')
+        ->join('leave_types', 'leave_types.id', '=', 'leave_applications.leave_type_id')
+        ->select('users.*', 'leave_applications.*', 'leave_types.name as leave_type_name');
+
+        if($request->get('name') != '') {
+            $query->where('users.name','like','%'.$search_name.'%');
+        }
+
+        if($request->get('date_from') != '' && $request->get('date_to') != '') {
+            $query->wherebetween('leave_applications.date_from', [$date_from, $date_to]);
+        }
+
+        if($request->get('leave_type') != '') {
+            $query->where('leave_applications.leave_type_id','like','%'.$leave_type.'%');
+        }
+
+        if($request->get('leave_status') != '') {
+            if($request->get('leave_status') == 'PENDING') {
+                $query->where('leave_applications.status','like','%PENDING_1%');
+                $query->orwhere('leave_applications.status','like','%PENDING_2%');
+                $query->orwhere('leave_applications.status','like','%PENDING_3%');
+            }
+            if($request->get('leave_status') == 'DENIED') {
+                $query->where('leave_applications.status','like','%DENIED_1%');
+                $query->orwhere('leave_applications.status','like','%DENIED_2%');
+                $query->orwhere('leave_applications.status','like','%DENIED_3%');
+            }
+            else {
+                $query->where('leave_applications.status','like','%'.$leave_status.'%');
+            }
+        }
+
+        $users = $query->get();
+        dd($users);
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->getStyle('A')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+        $sheet->setCellValue('A1', 'name');
+        $rows = 2;
+
+        $countapp = count($query);
+        dd($countapp);
+        $count = 1;
+
+        for($d=0; $d<$countapp; $d++) {
+            $sheet->getColumnDimension('A')->setAutoSize(true);
+            $sheet->setCellValue('A' . $rows, $users->name);
+            
+            $rows++;
+
+        }
+
+        $writer = new Xlsx($spreadsheet);
+        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment; filename="Leave_Applications.xlsx"');
+        $writer->save("php://output");
+
+        }
 }
--- resources/views/excel/transfer.blade.php
+++ resources/views/excel/transfer.blade.php
@@ -1,6 +1,10 @@
 @extends('adminlte::page')
 
 @section('content')
+<style>
+    .buttonStat {width: 100px;}
+    .card {margin: 0 auto; float: none; margin-bottom: 20px;}
+</style>
 @if ($errors->any())
 <div class="alert alert-danger">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
@@ -17,29 +21,194 @@
     <strong>{{ $message }}</strong>
 </div>
 @endif
-<div class="container">
-    <div class="card mt-4">
+<div class="mt-2 col-md-12">
+    <div class="card">
         <div class="card-header bg-teal">
             Import Excel
         </div>
         <div class="card-body">
-            <div class="float-left">
-                <form class="form-inline" action="{{route('excel_import')}}" method="post"
-                    enctype="multipart/form-data">
-                    {{csrf_field()}}
-                    <div class="form-group">
-                        <input type="file" class="form-control" name="import_file" />
+            <div class="panel panel-default">
+                <div class="row">
+                    <div class="card col-8">
+                        <div class="card-body">
+                            <form id="btnSearch" class="input-horizontal" action="{{ route('search') }}" method="get">
+                                <div class="form-inline form-group">
+                                    <!-- Name -->
+                                    <div class="input-group col-6">
+                                        <input type="search" name="name" placeholder="Name" value="{{ isset($search_name)? $search_name: '' }}" class="form-control">
+                                    </div>
+                                    <!-- Leave Type -->
+                                    <div class="input-group col-6">
+                                        <select class="form-control" name="leave_type">
+                                            <option value="" disabled selected>Select Leave Type</option>
+                                            <option value="1">Annual</option>
+                                            <option value="2">Calamity</option>
+                                            <option value="3">Sick</option>
+                                            <option value="4">Hospitalization</option>
+                                            <option value="5">Compassionate</option>
+                                            <option value="6">Emergency</option>
+                                            <option value="7">Marriage</option>
+                                            <option value="8">Maternity</option>
+                                            <option value="8">Paternity</option>
+                                            <option value="8">Traning</option>
+                                            <option value="8">Unpaid</option>
+                                            <option value="8">Replacement</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-inline form-group">
+                                    <!-- Date From -->
+                                    <div class="input-group col-6">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="fa fa-calendar-day"></i>
+                                            </span>
+                                        </div>
+                                        <input placeholder="Leave From" class="form-control" type="text" onfocus="(this.type='date')" value="{{ isset($date_from)? $date_from: '' }}" name="date_from">
+                                    </div>
+                                    <!-- Status -->
+                                    <div class="input-group col-6">
+                                        <select class="form-control" name="leave_status">
+                                            <option value="" disabled selected>Select Status</option>
+                                            <option value="PENDING">In Progress</option>
+                                            <option value="APPROVED">Approved</option>
+                                            <option value="CANCELLED">Cancelled</option>
+                                            <option value="DENIED">Rejected</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-inline form-group">
+                                    <!-- Date To -->
+                                    <div class="input-group col-6">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="fa fa-calendar-day"></i>
+                                            </span>
+                                        </div>
+                                        <input placeholder="Leave To" class="form-control" type="text" onfocus="(this.type='date')" value="{{ isset($date_to)? $date_to: '' }}" name="date_to">
+                                    </div>
+                                    <div class="d-flex justify-content-end col-6">
+                                        <button onclick="importShow()" class="btn btn-warning mr-1">Import</button>
+                                        <button form="btnExport" type="submit" class="btn btn-warning mr-1">Export</button>
+                                        <button form="btnSearch" type="reset" class="btn btn-primary mr-1">Reset</button>
+                                        <button form="btnSearch" type="submit" class="btn btn-primary">Search</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <form id="btnExport" action="{{ route('excel_export') }}" enctype="multipart/form-data">
+                                <input type="hidden" name="excel_name" value="{{isset($search_name)? $search_name: ''}}">
+                                <input type="hidden" name="excel_date_from" value="{{isset($date_from)? $date_from: ''}}">
+                                <input type="hidden" name="excel_date_to" value="{{isset($date_to)? $date_to: ''}}">
+                                <input type="hidden" name="excel_leave_type" value="{{isset($leave_type)? $leave_type: ''}}">
+                                <input type="hidden" name="excel_leave_status" value="{{isset($leave_status)? $leave_status: ''}}">
+                            </form>
+                        </div>
                     </div>
-                    <button style="margin-left: 10px;" class="btn btn-success mr-2" type="submit">Import</button>
-                </form>
+                    <div class="card col-4">
+                        <div class="card-body">
+                        <table class="table table-sm table-bordered table-striped table-hover">
+                            <thead>
+                            <tr>
+                                <th colspan="2">Leave Applications</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>Approved</td>
+                                <td>{{ $count_approve }}</td>
+                            </tr>
+                            <tr>
+                                <td>Pending</td>
+                                <td>{{ $count_pending }}</td>
+                            </tr>
+                            <tr>
+                                <td>Rejected</td>
+                                <td>{{ $count_reject }}</td>
+                            </tr>
+                            </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="card col-12 d-none" id="importCard">
+                    <div class="card-body">
+                        <form class="form-inline" action="{{ route('excel_import') }}" method="post">
+                            <div class="form-group">
+                                <input type="file" class="form-control" name="import_file" />
+                            </div>
+                            <button style="margin-left: 10px;" class="btn btn-success mr-2" type="submit">Import</button>
+                        </form>
+                    </div>
+                </div>
+                @if ($users->count() > 0)
+                    <h6><strong>Displaying {{$users->count()}} out of {{$users->total()}} records.</strong></h6>
+                @endif
+                <table class="table table-sm table-bordered table-striped table-hover">
+                <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>@sortablelink('name', 'Name')</th>
+                    <th>Day(s)</th>
+                    <th>Type</th>
+                    <th width="10%">@sortablelink('date_from', 'To Date')</th>
+                    <th width="10%">@sortablelink('date_from', 'From Date')</th>
+                    <th width="10%">@sortablelink('date_from', 'Resume Date')</th>
+                    <th>Reason</th>
+                    <th>Status</th>
+                    <th width="10%">@sortablelink('created_at', 'Apply Date')</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php $count = 0;?>
+                @foreach($users as $row)
+                <tr>
+                    <td>{{ ++$count }}</td>
+                    <td>{{ $row->name }}</td>
+                    <td>{{ $row->total_days }}</td>
+                    <td>{{ $row->leave_type_name }}</td>
+                    <td>{{ $row->date_from }}</td>
+                    <td>{{ $row->date_to }}</td>
+                    <td>{{ $row->date_resume }}</td>
+                    <td align="center">
+                        <button type="button" class="btn btn-sm btn-info" data-toggle="popover" data-trigger="focus" title="Details" 
+                        data-content="{{ $row->reason }}">View</button>
+                    </td>
+                    <td align="center">
+                        @if ($row->status == "APPROVED" )
+                            <button type="button" class="btn buttonStat btn-sm btn-success" disabled>Approved</button>
+                        @elseif ($row->status == "CANCELLED")
+                            <button type="button" class="btn buttonStat btn-sm btn-warning" disabled>Cancelled</button>
+                        @elseif ($row->status == "DENIED_1" || $row->status == "DENIED_2" || $row->status == "DENIED_3")
+                            <button type="button" class="btn buttonStat btn-sm btn-danger" disabled>Rejected</button>
+                        @else
+                            <button type="button" class="btn buttonStat btn-sm btn-primary" disabled>In Progress</button>
+                        @endif
+                    </td>
+                    <td>{{\Carbon\Carbon::parse($row->created_at)->isoFormat('Y-MM-DD')}}</td>
+                </tr>
+                @endforeach
+                @if ($users->count() == 0)
+                    <tr align="center">
+                        <td colspan="10"><strong>No records found.</strong></td>
+                    </tr>
+                @endif
+                </tbody>
+                </table>
+                {!! $users->appends(\Request::except('page'))->render() !!}
+                </div>
             </div>
-            <div class="float-left">
-                <form action="{{route('excel_export')}}" enctype="multipart/form-data">
-                    <button class="btn btn-dark" type="submit">Export</button>
-                </form>
             </div>
         </div>
     </div>
 </div>
+<script>
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
 
+function importShow() {
+  $('#importCard').removeClass("d-none");
+}
+
+</script>
 @endsection
