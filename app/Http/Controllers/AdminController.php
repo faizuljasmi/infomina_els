7f23f19d384c7c5535d1e8460f05f74e2e2b4764
gobhinrao <gobhinrao18@gmail.com>
2020-09-29 17:20:41 +0800

    Leave Balance Report alteration

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -496,6 +496,8 @@ class AdminController extends Controller
         $annual_total = User::leftjoin('leave_entitlements', 'leave_entitlements.emp_type_id', '=', 'users.emp_type_id')
         ->where('leave_entitlements.leave_type_id', '=', '1' )->get();
 
+        $annual_total = User::leftjoin('brought_forward_leaves', 'brought_forward_leaves.user_id', '=', 'users.user_id')->get();
+
         $calamity = User::leftjoin('leave_balances', 'leave_balances.user_id', '=', 'users.id')
         ->where('leave_balances.leave_type_id', '=', '2' )->get();
 
@@ -563,6 +565,8 @@ class AdminController extends Controller
         $sheet->setCellValue('O1', 'Replacement');
         $sheet->getStyle('P')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
         $sheet->setCellValue('P1', 'Total Annaul Ent');
+        $sheet->getStyle('Q')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+        $sheet->setCellValue('Q1', 'Total Brought Forw');
         $rows = 2;
 
         $countapp = count($annual);
@@ -626,6 +630,10 @@ class AdminController extends Controller
             if ( $annual_total ) {
                 $sheet->getColumnDimension('P')->setAutoSize(true);
                 $sheet->setCellValue('P' . $rows, $annual_total[$d]->no_of_days);
+            }
+            if ( $brought_forw ) {
+                $sheet->getColumnDimension('Q')->setAutoSize(true);
+                $sheet->setCellValue('Q' . $rows, $brought_forw[$d]->no_of_days);
             }
             $rows++;
         }
