afffed68866bd8f65c3f06ddcdb3882c8aed8e08
faizuljasmi <jasmifaizul@gmail.com>
2020-12-22 16:56:24 +0800

    faizul added

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -144,7 +144,7 @@ class RegistrationController extends Controller
         $leaveBal = LeaveBalance::orderBy('leave_type_id', 'ASC')->where('user_id', '=', $user->id)->get();
         $leaveTak = TakenLeave::orderBy('leave_type_id', 'ASC')->where('user_id', '=', $user->id)->get();
 
-        $ann_taken_first_half = LeaveApplication::where('user_id',$user->id)->where('status','Approved')->where('leave_type_id',1)->whereBetween('created_at' ,'2021-01-01', '2021-06-30')->get();
+        $ann_taken_first_half = LeaveApplication::where('user_id',$user->id)->where('status','Approved')->where('leave_type_id',1)->whereBetween('created_at' ,['2021-01-01', '2021-06-30'])->get();
         $total_ann_taken_first_half = 0;
         foreach($ann_taken_first_half as $ann){
             $total_ann_taken_first_half += $ann->total_days;
