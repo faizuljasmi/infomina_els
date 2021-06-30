7c866eb0341c1d17097caa123ab977b47990ed2e
faizuljasmi <faizuljasmi@users.noreply.github.com>
2021-06-30 17:48:22 +0800

    sso login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -37,5 +39,21 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    
+    public function logout(Request $request)
+    {
+        $user = Auth::user();
+        if ($user->user_type == "Admin") {
+            $this->guard()->logout();
+
+            $request->session()->invalidate();
+
+            return redirect('/admin-login');
+        } else {
+            $this->guard()->logout();
+
+            $request->session()->invalidate();
+
+            return redirect("http://128.199.123.181/app");
+        }
+    }
 }
