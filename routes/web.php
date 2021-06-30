af9a5ea0a33d789ea371edddc1f21d88db8402e7
faizuljasmi <faizuljasmi@users.noreply.github.com>
2021-06-30 15:09:12 +0800

    sso login

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,43 @@ Route::get('/sso-login/{token}', 'AdminController@sso_login');
 Route::get('/sso-logout/{token}', 'AdminController@sso_logout');
 //Route::get('/mobile/notification', 'LeaveApplicationController@mobile_notification');
 
-Auth::routes();
+//Auth::routes();
+  Route::post('login', [
+    'as' => '',
+    'uses' => 'Auth\LoginController@login'
+  ]);
+  Route::post('logout', [
+    'as' => 'logout',
+    'uses' => 'Auth\LoginController@logout'
+  ]);
+  
+  // Password Reset Routes...
+  Route::post('password/email', [
+    'as' => 'password.email',
+    'uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail'
+  ]);
+  Route::get('password/reset', [
+    'as' => 'password.request',
+    'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm'
+  ]);
+  Route::post('password/reset', [
+    'as' => 'password.update',
+    'uses' => 'Auth\ResetPasswordController@reset'
+  ]);
+  Route::get('password/reset/{token}', [
+    'as' => 'password.reset',
+    'uses' => 'Auth\ResetPasswordController@showResetForm'
+  ]);
+  
+  // Registration Routes...
+//   Route::get('register', [
+//     'as' => 'register',
+//     'uses' => 'Auth\RegisterController@showRegistrationForm'
+//   ]);
+//   Route::post('register', [
+//     'as' => '',
+//     'uses' => 'Auth\RegisterController@register'
+//   ]);
 
 // Route::get('/home', function() {
 //     return view('home');
