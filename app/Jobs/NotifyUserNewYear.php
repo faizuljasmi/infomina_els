8596c2ec85f1436d958fecec139da34754007660
jasmifaizul <jasmifaizul@gmail.com>
2022-12-22 11:09:38 +0800

    queue

--- app/Jobs/NotifyUserNewYear.php
+++ app/Jobs/NotifyUserNewYear.php
@@ -14,6 +14,7 @@ class NotifyUserNewYear implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $user;
     /**
      * Create a new job instance.
      *
